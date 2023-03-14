54447c9b36a228d9470eacb213e13acccd143eec
me <92444971+deigojojlo@users.noreply.github.com>
2023-03-14 21:57:00 +0100

    Update FTK.py

--- FTK.py
+++ FTK.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from random import randint 
+from random import randint
 
 class Mine() :
     def __init__(self) -> None:
@@ -12,7 +12,7 @@ class Mine() :
         self.l2 = Frame(self.fen)
         self.l3 = Frame(self.fen)
         self.l = [self.l1,self.l2,self.l3]
-        self.clavier = [["a","z","e","r","t","y","u","i","o","p"], 
+        self.clavier = [["a","z","e","r","t","y","u","i","o","p"],
              ["q","s","d","f","g","h","j","k","l","m"],
              ["w","x","c","v","b","n"]]
         self.lfin = Label(self.fen , text = "", bg = '#D2D2D2')
@@ -41,31 +41,39 @@ class Mine() :
                 self.clavier1[i].append(0)
                 self.clavier1[i][k] = Label(self.l[i], text = self.clavier[i][k].upper(), bg ="#D2D2D2", font=("calibri", 15), width=10)
                 self.clavier1[i][k].grid(row = i , column = k)
-            
+
     def start(self):
         self.case = randint(1,26)
         self.essai = []
-    
+        self.info = False
+
     def place(self,lettre) :
         for i in range(3):
             for k in range(len(self.clavier[i])) :
                 if lettre == self.clavier[i][k].upper():
                     return (i,k)
-            
+
     def get_entry(self,event):
-        m = str(self.entry.get())
+        m = str(self.entry.get())[0].lower()
         self.entry.delete(0,1)
         if m not in self.essai and len(m) == 1 and m in self.clavier[0]+self.clavier[1]+self.clavier[2]:
             dix,uni = self.place(m.upper())
             if m.upper() == chr(self.case+64) :
                 self.clavier1[dix][uni].config(bg = "#00FF00")
                 self.lfin.config(text = "gagné")
+                self.essai = self.clavier1[0]+self.clavier1[1]+self.clavier1[2]
+                self.info = True
             else :
                 if m not in self.essai :
-                    self.clavier1[dix][uni].config(bg = "#FF0000")
-                    self.essai.append(m)
-                    if len(self.essai) == 13 : 
+                    if not self.info:
+                        self.clavier1[dix][uni].config(bg = "#FF0000")
+                        self.essai.append(m)
+                    if len(self.essai) >= 13 :
                         self.lfin.config(text = "perdu")
+                        self.essai = self.clavier1[0]+self.clavier1[1]+self.clavier1[2]
+                        self.info = True
+
+
 
     def ready(self):
         self.fen.bind('<Return>',self.get_entry)
